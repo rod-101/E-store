3591589b42be6eba05f7a9db2438229446925159
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2024-07-30 00:50:51 +0800

    add displayProducts() to display products on screen

--- index.js
+++ index.js
@@ -13,26 +13,51 @@ function addProduct() {
     let qntyInStock = document.querySelector('#pInStock');
     let newProd;
 
-    function product(name, price, qntyInStock) {
+    function product(name, price,   stock) {
         return {
             name,
             price, 
-            qntyInStock
+            stock
         }
     }
 
-    //logic of adding product
+    //add product if fields are not empty
     if(pName.value && price.value && qntyInStock.value) {
         newProd = product(pName.value, price.value, qntyInStock.value)
         products.push(newProd)
+        pName.value = price.value = qntyInStock.value = null;
         console.log(products)
     } else {
         window.prompt('Please fill all the fields.')
     }
 }
 
-function viewProducts() {
-    for(let key in products) {
-        console.log(key + " " + products[key])
+//iterates through the properties of the objects
+//in the products array
+function displayProducts() {
+    let main = document.querySelector('#products');
+    let prod_container;
+    
+    //clear contents first
+    main.textContent = ''
+    
+    let displayProductDataInContainer = function(i, prop,container) {  
+        let content = document.createTextNode(`${prop}: ${products[i][prop]} `)
+        container.appendChild(content)
+        
+        main.appendChild(container)
+        console.log(`${products[i][prop]}`)
+    }
+
+    for (let i = 0; i < products.length; i++) {
+        prod_container = document.createElement('div')
+        
+        for (let prop in products[i]) {
+            displayProductDataInContainer(i, prop, prod_container)
+        }
     }
 }
+
+//loop throuh the array
+//the loop creates a div for each item in the array
+//in that div, the data from the object are displayed 
